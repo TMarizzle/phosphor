1c2d889c3ef370fdbb6390375485bb941926b1c5
EthanDunning <ethandunning2002@gmail.com>
2026-03-06 03:27:01 -0600

    made title font bigger

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -117,7 +117,7 @@ class App extends Component<any, AppState> {
         return (
             <>
                 <header className="phosphor-header">
-                    <span className="phosphor-header__title">PHOSPHOR v1.1</span>
+                    <span className="phosphor-header__title">PHOSPHOR v1.2</span>
 
                     <div className="phosphor-header__controls">
                         {uploadError && (
